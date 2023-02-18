92ca8f21ebc667384efc84f3dc2551c48b73e6ec
bilguun111101 <Mason@Masons-MacBook-Pro.local>
2023-02-18 23:09:08 +0800

    QR Code section Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from './components';
+import { Home, Login } from './components';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
   );
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
 
 export const Home = () => {
+  const [qr_url, setQr_url] = useState<string>("");
+  const [qr_image, setQr_image] = useState<string>("");
+  const generate = () => {
+    if (!qr_url) return;
+    setQr_image("");
+    const qr_code = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qr_url}`;
+    setQr_image(qr_code);
+  }
+
   return (
-    <section className='flex items-center justify-center w-full h-screen bg-sky-400'>
-      <div className='bg-white max-w-md rounded-lg py-4 px-6 h-64'>
+    <section className={`duration-200 flex items-center justify-center w-full h-screen bg-sky-400`}>
+      <div className={`bg-white max-w-md rounded-lg duration-500 py-4 px-6 h-${!qr_image ? 64 : 'auto'}`}>
         <header>
           <h1 className='text-xl font-medium'>QR Code Generator</h1>
           <p className='text-base text-indigo-800 mt-1.5'>Paste a url or enter text to create QR code</p>
         </header>
         <div className='my-5'>
-          <input type="text" name="" id="" placeholder='Enter text or url' className='w-full h-14 border outline-0 rounded-md pl-4' />
-          <button className='w-full h-14 border-none outline-0 rounded-md cursor-pointer bg-sky-400 mt-5 text-white active:bg-sky-500 duration-200'>Generate QR Code</button>
+          <input type="text" name="" id="" placeholder='Enter text or url' className='w-full h-14 border outline-0 rounded-md pl-4' onChange={e => setQr_url(e.target.value)} value={qr_url} />
+          <button 
+            className='w-full h-14 border-none outline-0 rounded-md cursor-pointer bg-sky-400 mt-5 text-white active:bg-sky-500 duration-200'
+            onClick={generate}
+          >Generate QR Code</button>
         </div>
-        <div className='py-8 flex items-center justify-center border border-zinc-300 rounded-md opacity-0 pointer-events-none'>
-          <img src="" alt="qr-code" />
+        <div className={`py-8 flex duration-500 items-center justify-center border border-zinc-300 rounded-md opacity-${!qr_image ? 0 : 1} pointer-events-none`}>
+          {
+            qr_image ? 
+              <img src={qr_image} alt="qr-code" />
+            : 
+              (<div className='w-36 h-36 flex items-center justify-center p-1.5'>
+                <div>
+                  <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
+              </div>)
+          }
         </div>
       </div>
     </section>
