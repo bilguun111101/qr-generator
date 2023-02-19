d4521c98c19162799ff4a947fd728976c49dcaa1
bilguun111101 <Mason@Masons-MacBook-Pro.local>
2023-02-19 14:53:06 +0800

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, Login } from './components';
+import { Home } from './components';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
   );
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,1 @@
 export * from "./Home/Home";
-export * from "./LogIn/Login";
-export * from "./SignUp/SignUp";
